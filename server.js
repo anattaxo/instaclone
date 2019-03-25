72244ab516b6a180a96bb4d0a48158d13ba4bcf8
anattaxo <liza.m.arnold@gmail.com>
2019-03-25 08:52:57 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const passport = require('passport');
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+
+// Body parser middleware
+app.use(bodyparser.urlencoded({ extended: false}));
+app.use(bodyparser.json());
+
+//Connect to db
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDb Connected'))
+  .catch(err => console.log(err));
+
+//passport middleware
+app.use(passport.initialize());
+//Passport config
+require('./config/passport')(passport);
+
+//Let's write our first route
+app.get('/', (req, res) => res.send('Hello!'));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5200;
+app.listen(port, () => console.log(`Server running on port ${port} `));
